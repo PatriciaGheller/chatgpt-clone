acef0c7d7c830a000e280f7456e47ff2499d30a2
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-06-20 14:36:32 -0300

    fix: ajustar componente Avatar svg para usar sprite

--- web/src/assets/avatar.js
+++ web/src/assets/avatar.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const Avatar = (props) =>{
   return(
-    <svg>
-
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" aria-hidden="true" data-rtl-flip="" class="icon-lg mx-2 text-token-text-secondary">
+      <use href="/cdn/assets/sprites-core-1a29ccf4.svg#38e54b" fill="currentColor">
+      </use>
     </svg>
   )
 }
