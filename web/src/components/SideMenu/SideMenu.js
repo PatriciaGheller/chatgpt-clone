980178e4f14321c31c00f8d9b6e8613850152a2b
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-06-20 12:05:21 -0300

    feat: adicionar SideMenu e integrar ao App

---
+++ web/src/components/SideMenu/SideMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './SideMenu.css';
+
+export const SideMenu = ()=> {
+  return(
+    <aside className='asidemenu'>
+      <div className="sidemenu-button">
+        <span>+</span>
+        Novo Chat
+      </div>
+    </aside>
+  )
+}
