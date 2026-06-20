cff1f23bba539a7ecad2512be91d0aebc30ec317
Patrícia Gheller <patricia.gheller.1985@gmail.com>
2026-06-20 14:53:26 -0300

    feat: adicionar estados de input e chatlog em App

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,4 @@
-import {useSate} from 'react';
+import {useState} from 'react';
 import './styles/App.css';
 import './styles/reset.css';
 
@@ -7,9 +7,19 @@ import {SideMenu} from './components/SideMenu/SideMenu';
 
 
 function App() {
+
+    const[input, setInput ] = useState("")
+    const[chatlog, setChatlog ] =useState([{
+        user:"gpt",
+        message:"Como posso te ajudar hoje ?"
+    }])
+
+
   return (
     <div className="App">
+
       <SideMenu></SideMenu>
+      
      <h1>App Works!! </h1>
     </div>
   );
